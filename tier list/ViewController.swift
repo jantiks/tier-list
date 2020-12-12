438ddfffbb742350f9325a7937ac93eb140f84d8
jantiks <58886410+jantiks@users.noreply.github.com>
2020-12-12 14:36:03 +0400

    Initial Commit

---
+++ tier list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tier list
+//
+//  Created by Tigran on 12/12/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
