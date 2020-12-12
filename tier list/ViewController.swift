8cbbe6898243f8bbf1e02387334be9087dd6eeba
jantiks <58886410+jantiks@users.noreply.github.com>
2020-12-12 15:32:11 +0400

    first interface is ready

--- tier list/ViewController.swift
+++ tier list/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var createButton: UIButton!
+    @IBOutlet var instructionsButton: UIButton!
+    @IBOutlet var draftsButton: UIButton!
+    @IBOutlet var upgradeButton: UIButton!
+    @IBOutlet var contactButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createButton.setTitle("Create", for: .normal)
+        createButton.setTitleColor(.white, for: .normal)
+        
+        instructionsButton.setTitle("Instrictions", for: .normal)
+        instructionsButton.setTitleColor(.white, for: .normal)
+
+        draftsButton.setTitle("Drafts", for: .normal)
+        draftsButton.setTitleColor(.white, for: .normal)
+
+        upgradeButton.setTitle("Upgrade", for: .normal)
+        upgradeButton.setTitleColor(.white, for: .normal)
+
+        contactButton.setTitle("Contact", for: .normal)
+        contactButton.setTitleColor(.white, for: .normal)
+    }
+    
+    @IBAction func createTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func instructionsTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func draftsTapped(_ sender: UIButton) {
     }
 
-
+    
+    @IBAction func upgradeTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func contactTapped(_ sender: UIButton) {
+    }
 }
 
